5753a01c7bd6ca86cfc64159e4a5fd0323f7cd23
KwaneleNdaba <kwanelendaba113@gmail.com>
2024-03-24 15:28:40 +0200

    initial commit

---
+++ petstorebackend/petstorebackend/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using petstorebackend.Entities;
+
+namespace petstorebackend.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ProductEntity> Product { get; set; }
+    }
+}
---
+++ petstorebackend/petstorebackend/DTOs/CreateUpdateProductDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace petstorebackend.DTOs
+{
+    public class CreateUpdateProductDTO
+    {
+        public string Title { get; set; }
+
+        public string Brand { get; set; }
+    }
+}
