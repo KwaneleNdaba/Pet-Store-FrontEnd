df698eaf5e6694b4fb0547e6bf5b09e1e3faec07
KwaneleNdaba <kwanelendaba113@gmail.com>
2024-03-24 15:52:49 +0200

    final commit

--- petstore/src/constants/url.constant.ts
+++ petstore/src/constants/url.constant.ts
@@ -1,1 +1,1 @@
-export const baseUrl = "https://backend-app-20240323130223.kindocean-86a13fad.eastus.azurecontainerapps.io/api/Products";
+export const baseUrl = "https://backend-app-20240323130223.kindocean-86a13fad.eastus.azurecontainerapps.io/api/Product";
